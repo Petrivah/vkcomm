90b46c22079130866ef3be11ff82668d65866dc5
Petrivah <petrivah@yandex.ru>
2019-06-26 23:01:14 +0300

    Added more debug info in logs

--- js/script.js
+++ js/script.js
@@ -15,7 +15,8 @@ let vue = new Vue({
         current_interval: null,
         comment_text: '',
         working: false,
-        debug: false
+        debug: false,
+        last_response: {}
     },
     methods: {
         deleteToken(id) {
@@ -25,6 +26,7 @@ let vue = new Vue({
             this.tokens.push(defaultToken);
         },
         startSpam() {
+            console.log('Спам начат!');
             let tokens = this.tokens.map(token => token.token),
                 address = this.post_address,
                 {
@@ -32,8 +34,7 @@ let vue = new Vue({
                         group_id,
                         post_id
                     }
-                } = address.match(/^http?s:\/\/(?:www)?vk\.com\/.*?wall(?<group_id>-?\d+?)_(?<post_id>\d+).*$/) ||
-                {
+                } = address.match(/^http?s:\/\/(?:www)?vk\.com\/.*?wall(?<group_id>-?\d+?)_(?<post_id>\d+).*$/) || {
                     groups: {
                         group_id: undefined,
                         post_id: undefined
@@ -41,27 +42,31 @@ let vue = new Vue({
                 },
                 interval = this.interval,
                 comment_text = encodeURIComponent(this.comment_text),
-                comment_count = 0;
+                repeat = 0;
             if (!group_id || !post_id) {
-                console.error('Неверный URL поста!');
+                console.error('Неверный URL поста!',
+                    `Ваш URL: ${address}`);
             } else if (tokens.filter(token => token.length < 84 || token.length > 86)[0]) {
-                console.error('Неверная длина одного из токенов!');
+                console.error('Неверная длина одного из токенов!',
+                    `Ваши токены: ${tokens.join(', ')}`);
             } else if (!this.interval.match(/^\d+$/)) {
-                console.error('Неверный интервал (пишите в мс)!');
+                console.error('Неверный интервал (пишите в мс)!',
+                    `Ваш интервал: ${interval}`);
             } else {
                 console.clear();
                 this.working = true;
-                let repeat = 0;
                 this.current_interval = setInterval(() => {
                     for (index in tokens) {
                         let i = index;
                         setTimeout(() => {
                             let component = encodeURIComponent(`https://api.vk.com/method/wall.createComment?owner_id=${group_id}&post_id=${post_id}&from_group=1&message=${comment_text}&access_token=${tokens[i]}&v=5.95`),
                                 url = `https://api.codetabs.com/v1/proxy?quest=${component}`;
-                            fetch(url);
+                            fetch(url).then(r => r.json()).then(json => {
+                                this.last_response = JSON.stringify(json.response)
+                            });
                             if (this.debug) {
                                 console.log({
-                                    url,
+                                    request_url: url,
                                     timeout: interval / tokens.length * i,
                                     full_timeout: repeat * interval + interval / tokens.length * i
                                 })
@@ -74,6 +79,8 @@ let vue = new Vue({
         },
         stopSpam() {
             clearInterval(this.current_interval);
+            console.log('Спам остановлен!');
+            if (this.debug) console.info(`Последний ответ от ВК: ${this.last_response}`);
             this.working = false;
         }
     },
